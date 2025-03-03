e0f769a642e12e6de282be2c340cc8d172fc05c6
acortinovis <adelio.cortinovis@gmail.com>
2025-03-03 09:40:15 -0800

    created dictionaries
    created dictionaries

---
+++ main.py
@@ -0,0 +1,26 @@
+#Make a dictionary called "cities". 
+#Use the names of three cities as keys in your dictionary.
+#Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city.
+#The keys for each city's dictionary should be something like "country, population, and fact".
+#Print the name of each city and all of the information you have stored about it.Make sure the final output is nicely formatted and neat.
+
+cities={
+    'Bergamo': {
+        'country':'Italy',
+        'population':'918,675',
+        'fact':'it has an ancient elevated part of the city',
+    },
+
+    'Bari':{
+        'country':'Italy',
+        'population':'623,000',
+        'fact':'Bari is a bustling commercial center and port.',
+    },
+
+    'Roma':{
+        'country':'Italy',
+        'population':'4,331,970',
+        'fact':'Rome has the famous Colosseum',
+    },
+
+    }
